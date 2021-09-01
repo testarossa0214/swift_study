7f2a3b0fb8997ca3f8d33b59aa986a6c2452da13
testarossa0214 <soundlight@live.jp>
2021-09-01 10:05:10 +0900

    swiftプログラミングの基本ルール

---
+++ BasicRule.playground/Contents.swift
@@ -0,0 +1,17 @@
+// UIkitのライブラリを読み込む
+import UIKit
+
+var str = "Hello, playground"
+
+// トークン　意味の最小の塊
+
+// トークンの間には半角スペースを入れる
+
+// プログラムの英数字や記号は必ず半角の文字で入力する
+
+var x = 2
+x * x
+
+var myName = "名前"
+
+print(myName)
